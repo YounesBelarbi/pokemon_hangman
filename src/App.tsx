2f9481699f2d47026dbbf50b000ba68ee9120090
younesbelarbi <ybelarbi@outlook.com>
2025-01-06 10:18:57 +0100

    chore: tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-  return <>pokemon hangman</>;
+  return (
+    <>
+      <div className="max-w-screen-xl mx-auto ">
+        <p className="text-3xl font-bold text-blue-500">pokemon hangman</p>
+      </div>
+    </>
+  );
 }
 
 export default App;
