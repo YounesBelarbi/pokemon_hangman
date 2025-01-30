af5d973eb1569abab3f0a7696fb2938834b08b37
younesbelarbi <ybelarbi@outlook.com>
2025-01-30 15:30:58 +0100

    feat: context api hook

---
+++ src/hook/UsePokemonApi.tsx
@@ -0,0 +1,142 @@
+// faire l'appel api
+// mettre une copie de la liste des pokemon dans le context pou dans le localstorage
+// choisir un pokemon au hasard dans la liste (et le retirer de la liste), le stocker soit dans le context soit dans le localstorage
+// dans App ??, si le pokemon est dans le localstorage, le prendre, sinon le prendre dans la liste
+// dans App ??, si la liste est vide refaire la requête pour recuperer une nouvelle liste de pokemon
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface Pokemon {
+  name: string;
+  image: string;
+}
+
+interface PokemonApiContextType {
+  context: any;
+  setContext: React.Dispatch<React.SetStateAction<any>>;
+  pokemon: any;
+  pokemonList: any[];
+  isError: boolean;
+  isLoading: boolean;
+  setPokemon: React.Dispatch<React.SetStateAction<Pokemon | null>>;
+  setPokemonList: React.Dispatch<React.SetStateAction<Pokemon[]>>;
+}
+
+const PokemonApiContext = createContext<PokemonApiContextType>({
+  context: {},
+  setContext: () => {},
+  pokemon: { name: "" },
+  pokemonList: [],
+  isError: false,
+  isLoading: false,
+  setPokemon: () => {},
+  setPokemonList: () => {},
+});
+
+const getRefreshContext = () => {
+  if (localStorage.getItem("pokemonApi") === null) {
+    localStorage.setItem("pokemonApi", JSON.stringify({}));
+  }
+};
+
+export const usePokemonContextApi = () => {
+  const {
+    context,
+    setContext,
+    pokemon,
+    pokemonList,
+    isError,
+    isLoading,
+    setPokemon,
+    setPokemonList,
+  } = useContext(PokemonApiContext);
+
+  return {
+    context,
+    setContext: (obj: PokemonApiContextType) => {
+      setContext({ ...context, ...obj });
+    },
+    resetContext: () => {
+      localStorage.removeItem("context");
+      setContext(getRefreshContext());
+    },
+    pokemon,
+    pokemonList,
+    isError,
+    isLoading,
+    setPokemon,
+    setPokemonList,
+  };
+};
+
+export const PokemonApiContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [context, setContext] = useState(getRefreshContext());
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchPokemon = async () => {
+    console.log("fetching pokemon");
+    setIsLoading(true);
+
+    try {
+      const response = await fetch("https://pokebuildapi.fr/api/v1/pokemon");
+      const data = await response.json();
+      setPokemonList(data);
+    } catch (error) {
+      console.error("Error fetching Pokemon data:", error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Fetch Pokemon data from API
+  useEffect(() => {
+    console.log("useEffect fetchPokemon");
+    // if (pokemonList.length === 0 && !isLoading) {
+    fetchPokemon();
+    // }
+  }, []);
+  console.log(pokemonList);
+
+  // Select a random Pokemon when the list is loaded
+  useEffect(() => {
+    if (pokemonList.length > 0 && !pokemon) {
+      const randomIndex = Math.floor(Math.random() * pokemonList.length);
+      const selectedPokemon = { ...pokemonList[randomIndex] };
+      selectedPokemon.name = selectedPokemon.name
+        .toUpperCase()
+        .normalize("NFD")
+        .replace(/[̀-ͯ]/g, "");
+      setPokemon(selectedPokemon);
+      setPokemonList((prevList) =>
+        prevList.filter((_, index) => index !== randomIndex)
+      );
+    }
+  }, [pokemonList, pokemon]);
+
+  console.log(pokemon);
+
+  return (
+    <PokemonApiContext.Provider
+      value={{
+        context,
+        setContext,
+        pokemon,
+        pokemonList,
+        isError,
+        isLoading,
+        setPokemon,
+        setPokemonList,
+      }}
+    >
+      {children}
+    </PokemonApiContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { PokemonApiContextProvider } from "./hook/usePokemonApi.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <PokemonApiContextProvider>
+      <App />
+    </PokemonApiContextProvider>
   </StrictMode>
 );
